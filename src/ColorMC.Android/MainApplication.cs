2865f0865432073d71e2f4586210dcdb19c42296
Color_yr <402067010@qq.com>
2023-10-11 00:18:12 +0800

    up
    重新添加pojav后端

--- src/ColorMC.Android/MainApplication.cs
+++ src/ColorMC.Android/MainApplication.cs
@@ -1,4 +1,6 @@
 ﻿using Android.App;
+using Android.Runtime;
+using Net.Kdt.Pojavlaunch;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +9,19 @@ using System.Threading.Tasks;
 
 namespace ColorMC.Android;
 
-//[Application(Name = "coloryr.colormc.load.MainApplication")]
-//public class MainApplication : Application
-//{
-    
-//}
+[Application(Name = "coloryr.colormc.load.MainApplication")]
+public partial class MainApplication : Application
+{
+    public MainApplication(IntPtr handle, JniHandleOwnership transfer)
+        : base(handle, transfer)
+    {
+        
+    }
+
+    public override void OnCreate()
+    {
+        base.OnCreate();
+
+        PojavApplication.Init(this);
+    }
+}
